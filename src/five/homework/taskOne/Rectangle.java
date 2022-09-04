8d60a3f0b5c052bf4d9f7ae1fa1734fb34bda32c
Lesia Kirat <lesiakirat@gmail.com>
2022-09-05 00:47:16 +0300

    Five Homework.

---
+++ src/five/homework/taskOne/Rectangle.java
@@ -0,0 +1,21 @@
+package five.homework.taskOne;
+
+public class Rectangle {
+    public double length;
+    public double width;
+
+    public Rectangle(double _length, double _width){
+        this.length = _length;
+        this.width = _width;
+    }
+    public Rectangle(){
+        length = 4;
+        width = 6;
+    }
+    public void area(){
+        System.out.println("Площа прямокутника = " + (length * width));
+    }
+    public void perimeter(){
+        System.out.println("Периметр прямокутника = " + (length + width) * 2);
+    }
+}
