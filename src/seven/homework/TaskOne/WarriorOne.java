5647f748b32d36d8f2a1028a0337869406b36288
Lesia Kirat <lesiakirat@gmail.com>
2022-09-12 10:20:52 +0300

    Six Homework

---
+++ src/seven/homework/TaskOne/WarriorOne.java
@@ -0,0 +1,10 @@
+package seven.homework.TaskOne;
+
+public class WarriorOne extends Warrior {
+    public WarriorOne() {
+        this.setName("Воїн 1");
+        this.setWeapon("Ніж");
+        this.setPower(200);
+        this.setHealth(1000);
+    }
+}
---
+++ src/seven/homework/TaskOne/WarriorThree.java
@@ -0,0 +1,10 @@
+package seven.homework.TaskOne;
+
+public class WarriorThree extends Warrior {
+    public WarriorThree() {
+        this.setName("Воїн 3");
+        this.setWeapon("Пістолет");
+        this.setPower(500);
+        this.setHealth(500);
+    }
+}
